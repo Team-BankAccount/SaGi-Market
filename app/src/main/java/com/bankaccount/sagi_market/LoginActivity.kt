9c103a31c00cefdf01a947fe31230e872f2c5dd4
정찬우 <105582057+Jung-Chanwoo@users.noreply.github.com>
2022-12-19 20:49:24 +0900

    LoginActivity xml파일 이름 변경

--- app/src/main/java/com/bankaccount/sagi_market/LoginActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.bankaccount.sagi_market
 import com.bankaccount.sagi_market.base.BaseActivity
 import com.bankaccount.sagi_market.databinding.LoginActivityBinding
 
-class LoginActivity : BaseActivity<LoginActivityBinding>(R.layout.login_activity) {
+class LoginActivity : BaseActivity<LoginActivityBinding>(R.layout.activity_login) {
 
     override fun viewSetting() {
 

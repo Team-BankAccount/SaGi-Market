9052592320c0ced77af65a3a821925174d960cbb
정찬우 <105582057+Jung-Chanwoo@users.noreply.github.com>
2022-12-20 19:32:55 +0900

    중간

--- app/src/main/java/com/bankaccount/sagi_market/FindPasswordActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/FindPasswordActivity.kt
@@ -1,4 +1,11 @@
 package com.bankaccount.sagi_market
 
-class FindPasswordActivity {
+import com.bankaccount.sagi_market.base.BaseActivity
+import com.bankaccount.sagi_market.databinding.ActivityFindPasswordBinding
+
+class FindPasswordActivity : BaseActivity<ActivityFindPasswordBinding>(R.layout.activity_find_password) {
+
+    override fun viewSetting() {
+        TODO("Not yet implemented")
+    }
 }

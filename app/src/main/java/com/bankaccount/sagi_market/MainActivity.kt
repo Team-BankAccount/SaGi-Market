4630113f9342376312b4bbbaa84b524e8fc3bc9b
Jeong MinSeok <ms0818000@gmail.com>
2022-12-20 09:52:00 +0900

    Splash,타이틀바 제거,로그인 로직 구현

--- app/src/main/java/com/bankaccount/sagi_market/MainActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.bankaccount.sagi_market
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn = findViewById<Button>(R.id.btn_Login)
+        loginBtn.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+        }
+
     }
 }

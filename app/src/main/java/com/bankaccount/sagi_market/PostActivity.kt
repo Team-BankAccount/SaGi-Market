c1d6791a57865fd337ebc93c8386201a1ce33525
Jeong MinSeok <ms0818000@gmail.com>
2022-12-20 16:01:06 +0900

    버그픽스 완료(gradle.properties에 android.enableJetifier=true 추가)

---
+++ app/src/main/java/com/bankaccount/sagi_market/PostActivity.kt
@@ -0,0 +1,11 @@
+package com.bankaccount.sagi_market
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}

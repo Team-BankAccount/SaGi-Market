40ae640c8161c357085e88407028ef214ac84740
yeongun130 <yeongunj625@gmail.com>
2022-12-21 09:47:50 +0900

    게시물작성

--- app/src/main/java/com/bankaccount/sagi_market/PostActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/PostActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_write)
+        setContentView(R.layout.activity_post)
     }
 }

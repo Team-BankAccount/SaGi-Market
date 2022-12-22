3b0939eb48ad9d74e6a7c9f02a234be77e30b6b6
Jeong MinSeok <ms0818000@gmail.com>
2022-12-22 13:48:34 +0900

    게시글 작성 구현,uid받아오기 구현

---
+++ app/src/main/java/Post/PostModel.kt
@@ -0,0 +1,11 @@
+package Post
+
+import android.icu.text.CaseMap.Title
+
+data class PostModel (
+    val title : String?,
+    val price : String?,
+    val detail : String?,
+    val uid : String?,
+    val time : String
+)
--- app/src/main/java/com/bankaccount/sagi_market/PostActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/PostActivity.kt
@@ -1,11 +1,34 @@
 package com.bankaccount.sagi_market
 
+import Post.PostModel
+import Util.FirebaseAuth
+import Util.FirebaseRef
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bankaccount.sagi_market.base.BaseActivity
+import com.bankaccount.sagi_market.databinding.ActivityPostBinding
+import com.bankaccount.sagi_market.databinding.ActivityRegisterBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
-class PostActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post)
+class PostActivity : BaseActivity<ActivityPostBinding>(R.layout.activity_post) {
+
+    override fun viewSetting() {
+        binding.btnSuccess.setOnClickListener{
+            val title = binding.etTitle.text.toString()
+            val price = binding.etPrice.text.toString()
+            val detail = binding.etDetail.text.toString()
+            val uid = FirebaseAuth.getUid()
+            val time = FirebaseAuth.getTime()
+
+            FirebaseRef.postRef
+                .push()
+                .setValue(PostModel(title,price,detail,uid,time))
+            Toast.makeText(this,"게시글 업로드 완료",Toast.LENGTH_SHORT).show()
+            finish()
+
+        }
     }
 }

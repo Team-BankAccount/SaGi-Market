04215ed39d52d92343457ce1ce0f883d655b64fd
정찬우 <105582057+Jung-Chanwoo@users.noreply.github.com>
2022-12-26 12:16:43 +0900

    :sparkles: 회원가입시 DB에 유저정보 저장

--- app/src/main/java/com/bankaccount/sagi_market/MainActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.bankaccount.sagi_market
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.bankaccount.sagi_market.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

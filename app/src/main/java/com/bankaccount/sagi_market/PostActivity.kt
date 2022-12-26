5c50a6868e2ef72898faa075f20388f8b52c60b0
Jeong MinSeok <ms0818000@gmail.com>
2022-12-26 18:15:14 +0900

    갤러리 접근권한 허락창 리뉴얼,이미지 업로드 기능 구현

--- app/src/main/java/com/bankaccount/sagi_market/PostActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/PostActivity.kt
@@ -5,6 +5,8 @@ import Util.FirebaseAuth
 import Util.FirebaseRef
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -17,10 +19,13 @@ import com.bankaccount.sagi_market.databinding.ActivityRegisterBinding
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+import java.util.jar.Manifest
 
 class PostActivity : BaseActivity<ActivityPostBinding>(R.layout.activity_post) {
 
     override fun viewSetting() {
+
         binding.btnSuccess.setOnClickListener{
             val title = binding.etTitle.text.toString()
             val price = binding.etPrice.text.toString()
@@ -28,37 +33,97 @@ class PostActivity : BaseActivity<ActivityPostBinding>(R.layout.activity_post) {
             val uid = FirebaseAuth.getUid()
             val time = FirebaseAuth.getTime()
 
+            val firebaseKey = FirebaseRef.postRef.push().key.toString()
+
             FirebaseRef.postRef
-                .push()
+                .child(firebaseKey)
                 .setValue(PostModel(title,price,detail,uid,time))
             Toast.makeText(this,"게시글 업로드 완료",Toast.LENGTH_SHORT).show()
-            imgUpload()
+
+            imgUpload(firebaseKey)
+
             finish()
         }
         binding.btnImg.setOnClickListener {
-            ActivityCompat.requestPermissions(this@PostActivity,
+            /*ActivityCompat.requestPermissions(this@PostActivity,
                 arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
             if (ContextCompat.checkSelfPermission(this@PostActivity.applicationContext,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-                        startActivityForResult(gallery,100)
+                val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                startActivityForResult(gallery,100)
 
             } else {
                 Toast.makeText(this,"갤러리 접근이 거부 되었습니다",Toast.LENGTH_SHORT).show()
-            }
+            }*/
+
+            requestPermission()
         }
     }
 
-    private fun imgUpload(){
+    private fun imgUpload(firebaseKey : String){
+        // Get the data from an ImageView as bytes
         val storage = Firebase.storage
         val storageRef = storage.reference
-        val mountainsRef = storageRef.child("mountains.jpg")
+        val mountainsRef = storageRef.child(firebaseKey+".png")
+
+
+        val imageView = binding.btnImg
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK&&requestCode == 100){
+        if(resultCode == RESULT_OK&&requestCode == 1004){
+            binding.btnImg.setImageURI(data?.data)
+        }
+    }
 
+    val PERMISSIONS_REQUEST_CODE = 100
+    var REQUIRED_PERMISSIONS = arrayOf<String>( android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    private fun requestPermission(){
+        var permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
+            }else{
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
+            }
+        }else{
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery,1004)            //권한 허용
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSIONS_REQUEST_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                    startActivityForResult(gallery,1004)
+                    //권한 허용
+                }else{
+                    //권한 거부됨
+                }
+                return
+            }
         }
     }
 }

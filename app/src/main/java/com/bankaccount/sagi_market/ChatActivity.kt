56f7d0f109e774cc619f1f264b84255ad89e2783
yeongun130 <yeongunj625@gmail.com>
2022-12-28 19:58:35 +0900

    채팅

---
+++ app/src/main/java/com/bankaccount/sagi_market/ChatActivity.kt
@@ -0,0 +1,14 @@
+package com.bankaccount.sagi_market
+
+import android.app.Activity
+import com.bankaccount.sagi_market.base.BaseActivity
+import com.bankaccount.sagi_market.databinding.ActivityMainBinding
+import util.FirebaseAuth
+
+class ChatActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_have_chatting) {
+
+    override fun viewSetting() {
+
+    }
+
+}

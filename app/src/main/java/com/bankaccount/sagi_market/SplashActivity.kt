7eff00d6378aaae3bb74f082f226c6c57dd50e34
정찬우 <105582057+Jung-Chanwoo@users.noreply.github.com>
2022-12-29 16:05:03 +0900

    Splash 간소화

--- app/src/main/java/com/bankaccount/sagi_market/SplashActivity.kt
+++ app/src/main/java/com/bankaccount/sagi_market/SplashActivity.kt
@@ -10,9 +10,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity( Intent(this, LoginActivity::class.java))
-            finish()
-        },3000)
+
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+
     }
 }
